fcab782a02464dc0d1da3858f666d4968ab700aa
Karthikeyan C <karthikeyan@hasura.io>
2024-12-16 16:39:10 +0530

    WIP: added the query tests under the `query` folder

--- relational/add_responses.sh
+++ relational/add_responses.sh
@@ -24,30 +24,27 @@ main() {
     info "Creating query directory..."
     mkdir query || error "Failed to create query directory"
 
-    # Find all directories in current path, excluding . and ..
-    dirs=$(find . -maxdepth 1 -type d ! -name "." ! -name ".." ! -name "query" ! -name ".git")
+    # List all directories before moving
+    info "Found directories:"
+    find . -maxdepth 1 -type d ! -name "." ! -name ".." ! -name "query" ! -name ".git" -exec basename {} \;
 
-    # Check if we found any directories to move
-    if [ -z "$dirs" ]; then
-        error "No directories found to move"
-    fi
-
-    # Counter for moved directories
-    moved=0
-
-    # Move each directory
-    for dir in $dirs; do
-        dir_name=$(basename "$dir")
-        info "Moving $dir_name to query/"
-        mv "$dir" "query/" || error "Failed to move $dir_name"
-        ((moved++))
+    # Move all directories except query and .git
+    for dir in */; do
+        # Skip query directory and .git directory
+        if [[ "$dir" != "query/" && "$dir" != ".git/" ]]; then
+            dir_name=${dir%/}  # Remove trailing slash
+            info "Moving $dir_name to query/"
+            mv "$dir_name" "query/" || error "Failed to move $dir_name"
+        fi
     done
 
-    info "Successfully moved $moved directories to query/"
+    # Show contents of query directory
+    info "Contents of query directory:"
+    ls -la query/
 
-    # Show the new structure
-    info "New directory structure:"
-    tree
+    # Show full directory structure
+    info "Final directory structure:"
+    ls -R
 }
 
 # Run the main function
